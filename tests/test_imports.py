fc7167bc17bcc03e3656448d76a1e3823361dc1e
Thalis Milisios <137193857+kambatzis@users.noreply.github.com>
2026-01-14 11:59:03 +0100

    Delete tests directory

--- tests/test_imports.py
+++
@@ -1,4 +0,0 @@
-def test_imports():
-    import facter  # noqa: F401
-    from facter.utils.seeding import seed_all, SeedConfig  # noqa: F401
-    from facter.tracking.mlflow import start_run, MLflowConfig  # noqa: F401
